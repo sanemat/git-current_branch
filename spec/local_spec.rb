161a97f07b03a3658a9ddd184a8b7ada01db3396
sanemat <o.gata.ken@gmail.com>
2013-04-20 21:50:40 +0900

    Fix spec

---
+++ spec/local_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Git::CurrentBranch::Local do
+  it { should be_true }
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,4 @@ $:.unshift File.expand_path('..', __FILE__)
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'rubygems'
 require 'bundler/setup'
-require 'git-current_branch'
+require 'git/current_branch'
