a776d29b914dd39efd73a27f70cf2eaa6675020e
sanemat <o.gata.ken@gmail.com>
2013-04-21 15:48:08 +0900

    Extract settings

--- spec/local_spec.rb
+++ spec/local_spec.rb
@@ -1,17 +1,19 @@
 require 'spec_helper'
 
 describe Git::CurrentBranch::Local do
+  let(:local) { described_class.new(path: File.dirname(__FILE__) + '/fixtures/testrepo.git/') }
+  let(:branch_name) { 'master' }
+  let(:same) { branch_name }
+  let(:not_same) { 'not_same' }
+
   it "" do
-    local = described_class.new(path: File.dirname(__FILE__) + '/fixtures/testrepo.git/')
-    expect(local.name).to eq('master')
+    expect(local.name).to eq(branch_name)
   end
 
   it '#name? same' do
-    local = described_class.new(path: File.dirname(__FILE__) + '/fixtures/testrepo.git/')
-    expect(local.name?('master')).to be_true
+    expect(local.name?(same)).to be_true
   end
   it '#name? not same' do
-    local = described_class.new(path: File.dirname(__FILE__) + '/fixtures/testrepo.git/')
-    expect(local.name?('not_same')).to be_false
+    expect(local.name?(not_same)).to be_false
   end
 end
