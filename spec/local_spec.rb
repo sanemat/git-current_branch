c89b656c97c42e07af46ed6a33f716ba861c1f6d
sanemat <o.gata.ken@gmail.com>
2013-04-21 15:45:34 +0900

    Add not same name

--- spec/local_spec.rb
+++ spec/local_spec.rb
@@ -6,8 +6,12 @@ describe Git::CurrentBranch::Local do
     expect(local.name).to eq('master')
   end
 
-  it '#name?' do
+  it '#name? same' do
     local = described_class.new(path: File.dirname(__FILE__) + '/fixtures/testrepo.git/')
     expect(local.name?('master')).to be_true
+  end
+  it '#name? not same' do
+    local = described_class.new(path: File.dirname(__FILE__) + '/fixtures/testrepo.git/')
+    expect(local.name?('not_same')).to be_false
   end
 end
