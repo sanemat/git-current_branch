2878487c378924e1c627a40d413c455b90d3cd86
sanemat <o.gata.ken@gmail.com>
2013-04-21 15:52:58 +0900

    Add description

--- spec/local_spec.rb
+++ spec/local_spec.rb
@@ -3,10 +3,15 @@ require 'spec_helper'
 describe Git::CurrentBranch::Local do
   let(:local) { described_class.new(path: File.dirname(__FILE__) + '/fixtures/testrepo.git/') }
   let(:branch_name) { 'master' }
-  let(:same) { branch_name }
-  let(:not_same) { 'not_same' }
+  let(:not_equal_branch_name) { 'not_equal_branch_name' }
 
   it { expect(local.name).to eq(branch_name) }
-  it { expect(local.name?(same)).to be_true }
-  it { expect(local.name?(not_same)).to be_false }
+
+  it '#name? equal branch_name' do
+    expect(local.name?(branch_name)).to be_true
+  end
+
+  it '#name? not equal branch_name' do
+    expect(local.name?(not_equal_branch_name)).to be_false
+  end
 end
