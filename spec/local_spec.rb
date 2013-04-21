dce95d77f0c6c7bf2762f6961c98731e4d51480b
sanemat <o.gata.ken@gmail.com>
2013-04-21 15:01:29 +0900

    First test

--- spec/local_spec.rb
+++ spec/local_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Git::CurrentBranch::Local do
-  it { should be_true }
+  it "" do
+    local = described_class.new(path: File.dirname(__FILE__) + '/fixtures/testrepo.git/')
+    expect(local.name).to eq('master')
+  end
 end
