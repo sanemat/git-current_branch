0f7eb95cae4f1dd218a0849c3eab0a489a8b65f5
sanemat <o.gata.ken@gmail.com>
2013-04-21 17:41:08 +0900

    Add name_equal_to command

--- lib/git/current_branch/cli.rb
+++ lib/git/current_branch/cli.rb
@@ -15,6 +15,17 @@ module Git
         local = Git::CurrentBranch::Local.new(path: Dir.pwd)
         puts local.name
       end
+
+      desc 'name_equal_to', 'Equal to branch name?'
+      def name_equal_to(compare)
+        local = Git::CurrentBranch::Local.new(path: Dir.pwd)
+        if local.name_equal_to? compare
+          exit 0
+        else
+          puts "This branch is #{local.name}, not equal to #{compare}"
+          exit 1
+        end
+      end
     end
   end
 end
