c413fa35cfa610c587dcf2134c7ef268f002ae20
sanemat <o.gata.ken@gmail.com>
2013-05-29 02:06:08 +0900

    Add coverall

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in git-current_branch.gemspec
 gemspec
 gem 'pry'
+gem 'coveralls', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,5 @@ $:.unshift File.expand_path('../../lib', __FILE__)
 require 'rubygems'
 require 'bundler/setup'
 require 'git/current_branch'
+require 'coveralls'
+Coveralls.wear!('rails')
