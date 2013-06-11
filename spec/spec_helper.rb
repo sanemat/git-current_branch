a3a65ea6dc1cda5953561cb5b76b2afb6ec1d2e5
Murahashi Sanemat Kenichi <o.gata.ken@gmail.com>
2013-06-11 16:58:22 +0900

    Move coveralls above all

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 $:.unshift File.expand_path('..', __FILE__)
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'rubygems'
-require 'bundler/setup'
-require 'git/current_branch'
 require 'coveralls'
 Coveralls.wear!('rails')
+require 'bundler/setup'
+require 'git/current_branch'
